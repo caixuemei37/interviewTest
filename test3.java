a5f1cf0faa27cecb660be4046fbbb8c35faae8a5
caixuemei37 <cxm1837@sina.cn>
2017-03-10 11:19:19 +0800

    Update test3.java

--- test3.java
+++ test3.java
@@ -1,44 +1,69 @@
 import java.util.concurrent.ConcurrentHashMap;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@Path("/lun")
 public class test3 {
 
-	private ConcurrentHashMap<Long, LUN> lunMap = new ConcurrentHashMap<Long, LUN>();
+	private SimpleMemoryPersistencyLayer persistentLayer = new SimpleMemoryPersistencyLayer();
 
-	@Path("/lun/create")
 	@POST
-	public void create(LUN... luns) {
-		for (LUN lun : luns) {
-			lunMap.put(lun.getId(), lun);
-		}
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response create(LUN lun) {
+		persistentLayer.create(lun);
+		return Response.ok().build();
 	}
 
-	@Path("/lun/delete")
 	@DELETE
-	public void delete(Long... ids) {
-		for (Long id : ids) {
-			lunMap.remove(id);
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/{id}")
+	public Response delete(@PathParam("id") long id) {
+		if (persistentLayer.get(id) == null) {
+			throw new IllegalStateException("No such LUN:" + id);
 		}
+
+		persistentLayer.delete(id);
+		return Response.ok().build();
 	}
 
-	@Path("/lun/update")
 	@PUT
-	public void update(LUN... luns) {
-		for (LUN lun : luns) {
-			lunMap.put(lun.getId(), lun);
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/{id}")
+	public Response update(@PathParam("id") long id, LUN lun) {
+		if (persistentLayer.get(id) == null) {
+			throw new IllegalStateException("No such LUN:" + id);
 		}
+
+		persistentLayer.update(lun);
+		return Response.ok().build();
 	}
 
-	@Path("/lun/retriveInfo")
 	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/{id}")
 	public LUN get(long id) {
-		return lunMap.get(id);
+		if (persistentLayer.get(id) == null) {
+			throw new IllegalStateException("No such LUN:" + id);
+		}
+
+		return persistentLayer.get(id);
 	}
 
+	@XmlRootElement(name = "lun")
+	@XmlAccessorType(XmlAccessType.PROPERTY)
 	public static class LUN {
 		private long id;
 		private long size;
@@ -68,5 +93,31 @@ public class test3 {
 			this.label = label;
 		}
 
+	}
+
+	private static class SimpleMemoryPersistencyLayer {
+		private ConcurrentHashMap<Long, LUN> lunMap = new ConcurrentHashMap<Long, LUN>();
+
+		public void create(LUN... luns) {
+			for (LUN lun : luns) {
+				lunMap.put(lun.getId(), lun);
+			}
+		}
+
+		public void delete(Long... ids) {
+			for (Long id : ids) {
+				lunMap.remove(id);
+			}
+		}
+
+		public void update(LUN... luns) {
+			for (LUN lun : luns) {
+				lunMap.put(lun.getId(), lun);
+			}
+		}
+
+		public LUN get(long id) {
+			return lunMap.get(id);
+		}
 	}
 }
